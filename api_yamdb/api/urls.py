c66ea9106be77a98b157b85da027fad3d0c927f5
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-16 16:42:39 +0300

    добавлены новые маршруты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+from rest_framework.routers import SimpleRouter
+
+from . import views
+router = SimpleRouter()
+
+router.register(r'/titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)',
+                views.ReviewsViewSet, basename='reviews')
+router.register(r'/categories/(?P<slug>\d+)',
+                views.CategoriesViewSet, basename='categories')
+router.register(r'/titles/(?P<title_id>\d+)',
+                views.TitlesViewSet, basename='titles')
+router.register(r'/titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments/(?P<comment_id>\d+)',
+                views.CommentsViewSet, basename='comments')
+router.register(r'/genres/(?P<slug>\d+)',
+                views.GenresViewSet, basename='genres')
 
 app_name = 'api'
 
 urlpatterns = [
-
+    path('v1/', include((router.urls, 'api')))
 ]
