15a0d878fd3a94080fa0c31ca18dfb58a8ecc9c5
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-17 20:33:08 +0300

    написаны новые permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,25 @@
 from rest_framework import permissions
 
 
-
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_staff:
+        if request.user.is_staff or request.user.role == 'admin':
             return True
-        
+
+
+class IsModerator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.role == 'moderator':
+            return True
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.username == obj.author:
+            return True
