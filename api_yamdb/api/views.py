e0213d4dd0b511f1028ce5d6e4db5a559a4b3bd7
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-17 12:21:58 +0300

    Настройка паджинации

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from .permissions import IsAdminOrReadOnly
 
 from .serializers import ReviewsSerializers, TitlesSerializers, CommentsSerializers, CatigoriesSerializers, GenresSerializers
-
+from .pagination import CustomPagination
 from reviews.models import Review, Title, Comment, Categorie, Genre
 
 
@@ -13,6 +13,7 @@ class ReviewsViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewsSerializers
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = CustomPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,12 +23,14 @@ class TitlesViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitlesSerializers
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = CustomPagination
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentsSerializers
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = CustomPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -37,9 +40,11 @@ class CategoriesViewSet(viewsets.ModelViewSet):
     queryset = Categorie.objects.all()
     serializer_class = CatigoriesSerializers
     permission_classes = (IsAdminOrReadOnly, )
+    pagination_class = CustomPagination
 
 
 class GenresViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenresSerializers
     permission_classes = (IsAdminOrReadOnly, )
+    pagination_class = CustomPagination
