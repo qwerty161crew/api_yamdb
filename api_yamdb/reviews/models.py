0f26ff1d57d157f24aa62c6394d63a18f32df4a7
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-17 11:48:27 +0300

    отладка бд. 9 тестов

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -36,23 +36,23 @@ class User(AbstractUser):
 
 
 class Categorie(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=300)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
     year = models.IntegerField()
     category = models.ForeignKey(
         Categorie, related_name='categories', on_delete=models.DO_NOTHING, null=True)
