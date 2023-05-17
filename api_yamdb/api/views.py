5948863d83b3a1f3c5be18d312db93fdcadfdcbd
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-17 20:32:46 +0300

    добавлены новые permissions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,6 @@
-from rest_framework import filters, mixins, viewsets, pagination
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly, IsAdminUser)
+from rest_framework import viewsets
+from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly, IsModerator
 from django.shortcuts import get_object_or_404
-from .permissions import IsAdminOrReadOnly
-
 from .serializers import ReviewsSerializers, TitlesSerializers, CommentsSerializers, CatigoriesSerializers, GenresSerializers
 from .pagination import CustomPagination
 from reviews.models import Review, Title, Comment, Categorie, Genre
@@ -12,7 +9,7 @@ from reviews.models import Review, Title, Comment, Categorie, Genre
 class ReviewsViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewsSerializers
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthorOrReadOnly, IsModerator)
     pagination_class = CustomPagination
 
     def perform_create(self, serializer):
@@ -22,18 +19,24 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 class TitlesViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitlesSerializers
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly, )
     pagination_class = CustomPagination
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentsSerializers
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthorOrReadOnly, IsModerator)
     pagination_class = CustomPagination
 
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
+
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        serializer.save(author=self.request.user,
+                        review=get_object_or_404(
+                            Review, pk=self.kwargs.get('review_id')))
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
