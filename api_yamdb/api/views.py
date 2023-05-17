a622851defb23671d072ac34f18a8afe4632f481
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-18 02:04:57 +0300

    изменен queryset

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,16 +4,25 @@ from django.shortcuts import get_object_or_404
 from .serializers import ReviewsSerializers, TitlesSerializers, CommentsSerializers, CatigoriesSerializers, GenresSerializers
 from .pagination import CustomPagination
 from reviews.models import Review, Title, Comment, Categorie, Genre
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class ReviewsViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewsSerializers
-    permission_classes = (IsAuthorOrReadOnly, IsModerator)
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly, IsModerator)
     pagination_class = CustomPagination
 
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, pk=title_id)
+        serializer.save(author=self.request.user, title=title)
+        return title.reviews.all()
 
 
 class TitlesViewSet(viewsets.ModelViewSet):
@@ -24,19 +33,18 @@ class TitlesViewSet(viewsets.ModelViewSet):
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentsSerializers
     permission_classes = (IsAuthorOrReadOnly, IsModerator)
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, pk=self.kwargs.get('title_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user,
                         review=get_object_or_404(
-                            Review, pk=self.kwargs.get('review_id')))
+                            Review, pk=self.kwargs.get('title_id')))
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
