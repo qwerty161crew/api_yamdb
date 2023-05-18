2c3ca71fc1eb0faf8bf0d063403047bbabf266da
Алексей Матвеев <qwerty161crew@gmail.com>
2023-05-18 11:28:32 +0300

    Revert "добавлен путь к статике"
    This reverts commit 0bca51d0928694b1aa2817637b85c5295e65ffa5.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -103,6 +103,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+STATIC_ROOT = BASE_DIR / 'static'
+
 AUTH_USER_MODEL = 'reviews.User'
